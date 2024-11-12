4566349e0f7a3c6aa652da51279cfec1d0c60a21
Sebastian Pietras <me@spietras.dev>
2024-11-12 18:38:00 +0100

    Restructured code and added i18n support (#7)

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,3 +1,4 @@
+// https://nextjs.org/docs/app/api-reference/next-config-js
 /** @type {import('next').NextConfig} */
 export default {
   // Change default build directory
@@ -10,6 +11,24 @@ export default {
 
   experimental: {
     // Optimize import from Mantine packages
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    optimizePackageImports: [
+      "@mantine/core",
+      "@mantine/hooks",
+      "@mantine/notifications",
+    ],
+
+    // Include SWC plugins
+    swcPlugins: [["@lingui/swc-plugin", {}]],
+  },
+
+  // Add support for loading .po files
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.po$/,
+      use: {
+        loader: "@lingui/loader",
+      },
+    });
+    return config;
   },
 };
--- src/src/actions/helloWorld/action.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { HelloWorldProps } from "./types";
-
-export async function helloWorld({}: HelloWorldProps = {}) {
-  return "Hello, World!";
-}
