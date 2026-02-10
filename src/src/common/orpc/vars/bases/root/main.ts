2324f19442cf78c8af44680ceef24184b02785f6
Sebastian Pietras <me@spietras.dev>
2026-02-10 21:13:45 +0100

    Improved oRPC errors (#25)

--- src/src/common/orpc/vars/bases/root/main.ts
+++ src/src/common/orpc/vars/bases/root/main.ts
@@ -3,4 +3,7 @@ import { Schemas } from "./schemas";
 
 export const orpcContractRootBase = orpcContractBuilder.errors({
   BAD_REQUEST: { data: Schemas.Errors.BadRequest },
+  CONFLICT: {},
+  INTERNAL_SERVER_ERROR: {},
+  NOT_FOUND: {},
 });
--- src/src/server/orpc/vars/procedures/vars/test/vars/validate/main.ts
+++ src/src/server/orpc/vars/procedures/vars/test/vars/validate/main.ts
@@ -7,18 +7,17 @@ import { localeMiddleware } from "../../../../../middleware/locale";
 
 export const validate = orpcServerRootBase.test.validate
   .use(localeMiddleware)
-  .handler(async ({ context, input }) => {
+  .handler(async ({ context, errors, input }) => {
     const { localization } = getLocalization({
       locale: context.localeMiddleware.locale,
     });
 
     const { data: getRandomJokeData } =
       await state.current.apis.icanhazdadjoke.getRandomJoke({
-        headers: {
-          Accept: "application/json",
-        },
-        throwOnError: true,
+        headers: { Accept: "application/json" },
       });
+
+    if (getRandomJokeData === undefined) throw errors.INTERNAL_SERVER_ERROR();
 
     return {
       message:
